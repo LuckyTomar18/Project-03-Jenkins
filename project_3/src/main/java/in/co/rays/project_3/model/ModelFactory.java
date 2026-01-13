03102ad165a81d7514e075977f79e7f646f698ee
Lucky <Luckytomar1801@gmail.com>
2026-01-13 11:35:42 +0530

    done
    Removed the getStaffModel method from ModelFactory.

--- project_3/src/main/java/in/co/rays/project_3/model/ModelFactory.java
+++ project_3/src/main/java/in/co/rays/project_3/model/ModelFactory.java
@@ -180,18 +180,5 @@ public final class ModelFactory {
 		return facultyModel;
 	}
 	
-	public StaffModelInt getStaffModel() {
-		StaffModelInt staffModel = (StaffModelInt) modelCache.get("staffmodel");
-		if (staffModel == null) {
-			if ("Hibernate".equals(DATABASE)) {
-				staffModel = new StaffModelHibImp();
-			}
-			if ("JDBC".equals(DATABASE)) {
-				staffModel = new StaffModelJDBCImp();
-			}
-			modelCache.put("staffModel", staffModel);
-		}
-
-		return staffModel;
-	}
+	
 }
