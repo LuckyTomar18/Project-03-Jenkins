cb3bc352af68db0bd05b9a7ba1f59a7cef2c191e
Lucky <luckytomar1801@gmail.com>
2026-01-16 17:57:53 +0530

    done

--- project_3/src/main/java/in/co/rays/project_3/util/HibDataSource.java
+++ project_3/src/main/java/in/co/rays/project_3/util/HibDataSource.java
@@ -17,17 +17,17 @@ public class HibDataSource {
 	private static SessionFactory sessionFactory = null;
 
 	public static SessionFactory getSessionFactory() {
-
 		if (sessionFactory == null) {
-			ResourceBundle rb = ResourceBundle.getBundle("in.co.rays.project_3.bundle.system");
+            ResourceBundle rb = ResourceBundle.getBundle("in.co.rays.project_3.bundle.system");
 
-			/*
-			 * String jdbcUrl = System.getenv("DATABASE_URL");
-			 *  if (jdbcUrl == null || jdbcUrl.trim().isEmpty()) { 
-			 *  jdbcUrl = rb.getString("url"); }
-			 */
-			sessionFactory = new Configuration().configure().buildSessionFactory();
-		}
+            String jdbcUrl = System.getenv("DATABASE_URL");
+            if (jdbcUrl == null || jdbcUrl.trim().isEmpty()) {
+                jdbcUrl = rb.getString("url");
+            }
+           
+            
+            sessionFactory = new Configuration().configure()
+            		.setProperty("hibernate.connection.url", jdbcUrl).buildSessionFactory();		}
 		return sessionFactory;
 	}
 
@@ -37,6 +37,8 @@ public class HibDataSource {
 		return session;
 
 	}
+	
+	
 
 	public static void closeSession(Session session) {
 
